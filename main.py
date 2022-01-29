e3ce124fa1d517a7c91149fad5e6db8cc8ee7b0c
noahelrimawi-fine <noah.elrimawi-fine@mail.mcgill.ca>
2022-01-29 11:08:27 -0500

    This is my first commit!

---
+++ main.py
@@ -0,0 +1,11 @@
+def main_wrapper():
+    # attributes are a thing!
+    print(f"This is the start of our python project. This function's name is {main_wrapper.__name__}")
+    # code here
+
+    #
+    print("This is the end of our Python project")
+
+
+if __name__ == "__main__":
+    main_wrapper()
